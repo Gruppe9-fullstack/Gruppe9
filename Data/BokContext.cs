df71c180c0936e4209027d108ad662529f37fd81
Gruppe9-fullstack <885809@usn.no>
2023-06-08 14:54:37 +0200

    5. Database kontekst filer
    Opprettet kontekst filer til databasen og lastet ned pakken Microsoft.EntityFrameworkCore.

---
+++ Data/BokContext.cs
@@ -0,0 +1,13 @@
+﻿using Gruppe9.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class BokContext : DbContext
+{
+    public DbSet<Bok> Bok { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Bibliotek_gruppe9.db");
+
+    }
+}
---
+++ Data/EksemplarContext.cs
@@ -0,0 +1,12 @@
+﻿using Gruppe9.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class EksemplarContext : DbContext
+{
+    public DbSet<Eksemplar> Eksemplar { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Bibliotek_gruppe9.db");
+    }
+}
